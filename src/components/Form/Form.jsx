b442bdd4b6c60a2105fbe2677c36dc29445ea8ee
Alheimurin <pasha.jmakov@gmail.com>
2023-09-13 16:54:51 +0500

    develop

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,19 +1,20 @@
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './form.css'
 import '../fonts/Fonts.css'
-import Confirm from '../popup/Confirm/Confirm'
+/* import Confirm from '../popup/Confirm/Confirm' */
+
 
 function Form (props) {
 
-    const [active, setActive] = useState(false)
+    /* const [active, setActive] = useState(false) */
 
     const [phoneNumber, setPhoneNumber] = useState ('')
     const [email, setEmail] = useState('')
-    const [validField, setValidField] = useState({
-        v: true, 
-        x: true
+    
+    const [value, setValue] = useState({
+        iniValue: 0
     })
 
     let validPhone = [{
@@ -22,7 +23,6 @@ function Form (props) {
     }]
     if (phoneNumber.length > 0) {
         validPhone = []
-        validField.v = false
     }
     if (phoneNumber.length === 0) {
         validPhone.phone = 'Телефон'
@@ -41,28 +41,51 @@ function Form (props) {
         validEmail.mark = '*'
     }
 
-    
+    /* const form = document.getElementById('form');
 
+    if (form) {
+        form.addEventListener('submit', formSend)
+    }
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let formData = new FormData(form);
+        console.log(formData)
+        let response = await fetch('sendmail.php', {
+            method: 'POST',
+            body: formData
+        });
+        if (response.ok) {
+            form.reset()
+        } else {
+            alert('ошибка')
+            e.stopPropagination()
+        }
+    } */
 
     return (
         <div className="form">
-            <form className='form__item' action="">
+            <form className='form__item' id='form' action="">
                 <div className="form__left">
                     <h1 className='form__left-text'>ФОРМА <span className='form__left-textGradient'>БЫСТРОЙ</span> ОБРАТНОЙ СВЯЗИ</h1>
                     <div className='form__left-inputs'>
                         <div className='inputs-form'>
                             <label className='inputs-labelLeft' htmlFor="">От кого</label>
-                            <input className='form__left-input wLabel' type="text" placeholder='Компания' />
-                            <img style={validField.v ? {display: 'none'} : {display: 'block'}} src="/UI/formV.svg" alt="" /> <img style={validField.x ? {display: 'none'} : {display: 'block'}} src="/UI/formX.svg" alt="" />
+                            <input onChange={(e)=>{setValue((e.target.value))}} className='form__left-input wLabel' type="text" placeholder='Компания' />
+                            <img style={value.length > 0 ? {display: 'block'} : {display: 'none'}} src="/UI/formV.svg" alt="" /> 
+                            <img style={value.length === 0 ? {display: 'block'} : {display: 'none'}} src="/UI/formX.svg" alt="" />
                         </div>
                         <div style={{display: 'flex'}}>
-                            <input className='form__left-input' type="text" placeholder='Имя'/>
-                            <img style={validField.v ? {display: 'none'} : {display: 'block'}} src="/UI/formV.svg" alt="" /> <img style={validField.x ? {display: 'none'} : {display: 'block'}} src="/UI/formX.svg" alt="" />
+                            <input onChange={(e)=>{setValue(e.target.value)}} className='form__left-input' type="text" placeholder='Имя'/>
+                            <img style={value.length > 0 ? {display: 'block'} : {display: 'none'}} src="/UI/formV.svg" alt="" /> 
+                            <img style={value.length === 0 ? {display: 'block'} : {display: 'none'}} src="/UI/formX.svg" alt="" />
                         </div>
                         <div className='req'>
-                            <input className='form__left-input' onChange={(e)=>setPhoneNumber(e.target.value)} id='phone' type="text" />
+                            <input onChange={(e)=>{setValue(e.target.value)}} className='form__left-input' onChange={(e)=>setPhoneNumber(e.target.value)} id='phone' type="text" />
                             <label className='requiredItem' htmlFor="phone">{validPhone.phone} <span style={{color:'red'}}>{validPhone.mark}</span></label>
-                            <img style={validField.v ? {display: 'none'} : {display: 'block'}} src="/UI/formV.svg" alt="" /> <img style={validField.x ? {display: 'none'} : {display: 'block'}} src="/UI/formX.svg" alt="" />
+                            <img style={value.length > 0 ? {display: 'block'} : {display: 'none'}} src="/UI/formV.svg" alt="" /> 
+                            <img style={value.length === 0 ? {display: 'block'} : {display: 'none'}} src="/UI/formX.svg" alt="" />
                         </div>
                         
                         <div className='inputs-form'>
@@ -70,13 +93,15 @@ function Form (props) {
                             <div className='required'>
                                 <input className='form__left-input wLabel' onChange={(e)=>setEmail(e.target.value)} id='email' type="text" />
                             <label className='requiredItem' htmlFor="email">{validEmail.email} <span style={{color:'red'}}>{validEmail.mark}</span></label> 
-                            <img style={validField.v ? {display: 'none'} : {display: 'block'}} src="/UI/formV.svg" alt="" /> <img style={validField.x ? {display: 'none'} : {display: 'block'}} src="/UI/formX.svg" alt="" />    
+                            <img style={value.length > 0 ? {display: 'block'} : {display: 'none'}} src="/UI/formV.svg" alt="" /> 
+                            <img style={value.length === 0 ? {display: 'block'} : {display: 'none'}} src="/UI/formX.svg" alt="" />    
                             </div>
                                     
                         </div>
                         <div style={{display: 'flex'}}>
-                            <input className='form__left-input' type="text" placeholder='Комментарий' />
-                            <img style={validField.v ? {display: 'none'} : {display: 'block'}} src="/UI/formV.svg" alt="" /> <img style={validField.x ? {display: 'none'} : {display: 'block'}} src="/UI/formX.svg" alt="" />
+                            <input onChange={(e)=>{setValue(e.target.value)}} className='form__left-input' type="text" placeholder='Комментарий' />
+                            <img style={value.length > 0 ? {display: 'block'} : {display: 'none'}} src="/UI/formV.svg" alt="" /> 
+                            <img style={value.length === 0 ? {display: 'block'} : {display: 'none'}} src="/UI/formX.svg" alt="" />
                         </div>
                         <div className='form__left-inputCheckbox'>
                             <input className='form__left-inputCheck' type="checkbox" defaultChecked />
@@ -120,15 +145,15 @@ function Form (props) {
                             </div>
                             
                             <div>
-                                <button onClick={(e)=>{setActive(true)}} className='form__submit' type='submit'>Отправить заявку</button>
+                                <button /* onClick={formSend} */ className='form__submit' type='submit'>Отправить заявку</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                <Confirm
+                {/* <Confirm
             active={active}
             setActive={setActive}
-            />
+            /> */}
             </form>
         </div>
     )
